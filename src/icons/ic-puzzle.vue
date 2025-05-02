c31c1cbaf828860b22553fce2b6567e5aa93ba5c
JenJenFenFen <fenjenjen@gmail.com>
2025-05-02 20:01:59 +0700

    feat: input icon (40%)

---
+++ src/icons/ic-puzzle.vue
@@ -0,0 +1,29 @@
+<template>
+  <svg
+    width="18"
+    height="19"
+    viewBox="0 0 18 19"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_2048_25420)">
+      <path
+        d="M9.00006 2.30005L6.30006 5.00005C4.50006 0.275049 -0.524939 5.30005 4.20006 7.10005L1.50006 9.80005L4.20006 12.5C6.00006 7.77505 11.0251 12.8 6.30006 14.6L9.00006 17.3L11.7001 14.6C13.5001 19.325 18.5251 14.3 13.8001 12.5L16.5001 9.80005L13.8001 7.10005C12.0001 11.825 6.97506 6.80005 11.7001 5.00005L9.00006 2.30005Z"
+        stroke="#2D2D2D"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_2048_25420">
+        <rect
+          width="18"
+          height="18"
+          fill="white"
+          transform="matrix(-1 0 0 1 18.0001 0.800049)"
+        />
+      </clipPath>
+    </defs>
+  </svg>
+</template>
