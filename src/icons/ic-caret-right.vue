c5bc8f82b68c4bb1b922cd9d511723b1aeae79cc
JenJenFenFen <fenjenjen@gmail.com>
2025-05-05 17:51:19 +0700

    feat: input icon(100%)

---
+++ src/icons/ic-caret-right.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg
+    width="19"
+    height="19"
+    viewBox="0 0 19 19"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M12.6125 9.3999L8.39375 13.297L8.39375 5.50279L12.6125 9.3999Z"
+      fill="currentColor"
+    />
+  </svg>
+</template>
---
+++ src/icons/ic-twitch.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg
+    width="18"
+    height="19"
+    viewBox="0 0 18 19"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M9.75 4.91424H8.67857V8.12852H9.75V4.91424Z"
+      fill="currentColor"
+    />
+    <path
+      d="M12.4286 8.12852V4.91424H11.3571V8.12852H12.4286Z"
+      fill="currentColor"
+    />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M4.92857 1.69995L2.25 4.37852V14.0214H5.46429V16.7L8.14286 14.0214H10.2857L15.1071 9.19995V1.69995H4.92857ZM11.8929 10.8071L14.0357 8.66424V2.77138H5.46429V10.8071H7.60714V12.95L9.75 10.8071H11.8929Z"
+      fill="currentColor"
+    />
+  </svg>
+</template>
