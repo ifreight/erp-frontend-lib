5a3373ec05f62a27b56782f4b0f006de0077650e
JenJenFenFen <fenjenjen@gmail.com>
2025-05-13 14:57:36 +0700

    feat: create sticky bar (100%)

--- src/components/i-sticky-bar.vue
+++ src/components/i-sticky-bar.vue
@@ -1,12 +1,32 @@
 <template>
-  <div class="i-sticky-bar">
-    test
+  <div
+    class="i-sticky-bar"
+    :style="{
+      'width': `calc(100% - ${widthSidebar})`,
+      'margin-left': `-${leftPosition}px`
+    }"
+  >
+    <div class="i-sticky-bar-content">
+      <slot name="content" />
+    </div>
+
+    <slot name="options" />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'IStickyBar'
+  name: 'IStickyBar',
+  props: {
+    leftPosition: {
+      type: Number,
+      default: 24
+    },
+    widthSidebar: {
+      type: String,
+      required: true
+    }
+  }
 }
 </script>
 
@@ -14,6 +34,10 @@ export default {
 @reference "@/assets/global.css";
 
 .i-sticky-bar {
-  @apply tw:bg-gray-900 tw:w-full;
+  @apply tw:fixed tw:bottom-8 tw:z-[1] tw:flex tw:justify-between tw:items-center tw:p-7 tw:bg-gray-900/70;
+
+  .i-sticky-bar-content {
+    @apply tw:text-xl tw:text-white tw:font-semibold;
+  }
 }
 </style>
--- src/views/StickyBarView.vue
+++ src/views/StickyBarView.vue
@@ -4,25 +4,62 @@
     <div class="tw:mb-12">
       <div class="tw:font-bold tw:bg-yellow-800 tw:text-xl">Normal sticky bar</div>
       <div class="tw:mt-6">
-        <!-- <i-checkbox
-          v-model="modelOne"
-          name="modelOneNormal"
-          label="Pilih opsi A"
-        /> -->
-        <i-sticky-bar />
+        <i-checkbox-group v-model="checkedList">
+          <i-checkbox
+            name="modelOne"
+            label="One"
+          />
+          <i-checkbox
+            name="modelTwo"
+            label="Two"
+          />
+        </i-checkbox-group>
+
+        <i-sticky-bar
+          v-if="checkedList.length > 0"
+          width-sidebar="20%"
+        >
+          <template #content>
+            {{ checkedList.length }} option(s) selected
+          </template>
+          <template #options>
+            <div class="tw:flex tw:gap-2.5">
+              <i-button text>
+                Cancel
+              </i-button>
+
+              <i-button class="tw:w-[150px]">
+                Click me!
+              </i-button>
+            </div>
+          </template>
+        </i-sticky-bar>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// import ICheckbox from '@/components/checkbox/i-checkbox.vue';
+import { ref } from 'vue';
+
+import IButton from '@/components/i-button.vue';
+import ICheckbox from '@/components/checkbox/i-checkbox.vue';
+import ICheckboxGroup from '@/components/checkbox/i-checkbox-group.vue';
 import IStickyBar from '@/components/i-sticky-bar.vue';
 
 export default {
   components: {
-    // ICheckbox,
+    IButton,
+    ICheckbox,
+    ICheckboxGroup,
     IStickyBar
+  },
+  setup() {
+    const checkedList = ref([])
+
+    return {
+      checkedList
+    }
   }
 }
 
